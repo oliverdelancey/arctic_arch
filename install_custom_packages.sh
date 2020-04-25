6058351eb3601358d0c7dbc359609c7a7a4261b3
Oliver Sandli <56598098+oliversandli@users.noreply.github.com>
2020-04-25 11:56:28 -0500

    Remove git

--- install_custom_packages.sh
+++ install_custom_packages.sh
@@ -4,7 +4,7 @@ sudo pacman -S figlet
 figlet "Installing Languages"
 sudo pacman -S python python-pip ruby ruby-irb clang
 figlet "Installing Utilities"
-sudo pacman -S git htop vtop neofetch
+sudo pacman -S htop vtop neofetch
 figlet "Installing zsh"
 sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
