0448473e049020d55996ce7cd9e56b57d14182fa
Oliver Sandli <56598098+oliversandli@users.noreply.github.com>
2020-06-13 16:28:57 -0500

    Update install_custom_packages.sh

--- install_custom_packages.sh
+++ install_custom_packages.sh
@@ -3,8 +3,9 @@
 sudo pacman -S figlet
 figlet "Installing Languages"
 sudo pacman -S python python-pip ruby ruby-irb clang
+curl https://sh.rustup.rs -sSf | sh
 figlet "Installing Utilities"
-sudo pacman -S htop vtop neofetch
+sudo pacman -S htop vtop neofetch git
 figlet "Installing zsh"
 sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
